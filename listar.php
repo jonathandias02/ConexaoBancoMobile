2fcc4e7285e783a33d05fd636b11dafb9fd428ae
Jonathan Almeida Dias <jonathan.a.dias@gmail.com>
2018-11-26 23:37:05 -0200

    Adicionado funcao de avaliacao ao listar instituiçoes

--- listar.php
+++ listar.php
@@ -13,5 +13,22 @@ while ($empresa = $query->fetch(PDO::FETCH_OBJ)){
         "ENDERECO" => $empresa->endereco);
     
 }
+for ($i = 0; $i < count($resultado); $i++){
+	$sql = "SELECT nota FROM t_avaliacao WHERE t_cliente_cnpj = :CNPJ";
+	$stmt = $conexao->prepare($sql);
+	$stmt->bindParam(':CNPJ', $resultado[$i]['CNPJ']);
+	$stmt->execute();
+	$notas = $stmt->fetchAll(PDO::FETCH_OBJ);
+	$total = 0;
+	for($j = 0; $j < count($notas); $j++){
+		$total += $notas[$j]->nota;
+	}	
+	if(count($notas) > 0){
+		$media = number_format($total / count($notas), 1);
+		$resultado[$i]['AVALIACAO'] = "$media";
+	}else{
+		$resultado[$i]['AVALIACAO'] = "S/A";
+	}
+}
 
 echo json_encode($resultado);
